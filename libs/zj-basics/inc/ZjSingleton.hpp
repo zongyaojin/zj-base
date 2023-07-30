acb4ef428eead5aef57a56611767c496dfbc9fc4
Zongyao Jin <zongyaojin@outlook.com>
2023-07-30 14:47:32 -0700

    cleans up singleton

--- libs/zj-basics/inc/ZjSingleton.hpp
+++ libs/zj-basics/inc/ZjSingleton.hpp
@@ -10,20 +10,36 @@
  * @see https://docs.microsoft.com/en-us/cpp/cpp/explicitly-defaulted-and-deleted-functions?view=msvc-160
  *
  * @author Zongyao Jin
- * @date 2023-7
+ * @date 2023-07-30
  */
 
 #pragma once
 
-#include <utility>
-
-template <typename T, typename... Args>
+/**
+ * @warning If the child class needs constructor arguments, then you SHOULD design another wrapper class with the class that requires
+ * constructor arguments as a member variable. For example, you can have a `unique_ptr<ClassRequiresConstructorArgument>` member variable,
+ * and an `Singleton::getInstance().init(...)` function to deal with the initialization of ClassRequiresConstructorArgument.
+ *
+ * @warning If you apply variadic template arguments to the `getInstance(...)` function, it's very confusing since only the input arguments
+ * used by the first time `Singleton::getInstance(real...)` is called are applied to constructor the instance. The following calls still
+ * have to provide some dummy arguments such as `Singleton::getInstance(any_dummy...)` to match the function signature, they these dummy
+ * arguments could potentially be anything, and will not be used since the instance was already constructed. Yet calling
+ * `Singleton::getInstance(any_dummy...)` makes it very difficult to the reader to trace what are the real constructor arguments used by the
+ * instance specially in a program with multiple threads.
+ *
+ * @warning To summarize, with an `Singleton::getInstance().init(...)` function, the whole program can call it only once, and never use
+ * it again; which make the initialization easy to trace. In the contrast, calling `Singleton::getInstance(real_or_dummy...)` all over the
+ * place isn't a good design.
+ *
+ * @see https://stackoverflow.com/a/1051076
+ */
+template <typename ChildType>
 class ZjSingleton
 {
 public:
-    static T& getInstance(Args&&... args)
+    static ChildType& getInstance()
     {
-        static T s_instance(std::forward<Args>(args)...);
+        static ChildType s_instance;
         return s_instance;
     }
 
--- tests/testZjSingleton.cpp
+++ tests/testZjSingleton.cpp
@@ -11,32 +11,9 @@ protected:
     int m_i {0};
 };
 
-/// @note the getInstance in this case is a little confusing since after the first time, you can pass any argument value, it doesn't matter,
-/// because the static instance is already constructed; it won't construct again with the new value
-class ZjSingletonCtrTest : public ZjSingleton<ZjSingletonCtrTest, int>
-{
-    friend class ZjSingleton<ZjSingletonCtrTest, int>;
-
-public:
-    inline int addToBase(const int a) { return m_base += a; }
-    inline int base() const { return m_base; }
-
-protected:
-    ZjSingletonCtrTest(int base)
-    : m_base(base) {};
-    virtual ~ZjSingletonCtrTest() = default;
-
-    int m_base {0};
-};
-
 void addToSingleton(const int i)
 {
     ZjSingletonTest::getInstance().add(i);
-}
-
-void addToSingletonCtrTest(const int i)
-{
-    ZjSingletonCtrTest::getInstance(0).addToBase(i);
 }
 
 TEST(TestZjSingleton, MultipleAccess)
@@ -53,25 +30,4 @@ TEST(TestZjSingleton, MultipleAccess)
     addToSingleton(3);
     EXPECT_EQ(ZjSingletonTest::getInstance().i(), 13);
     EXPECT_EQ(instance.i(), 13);
-}
-
-TEST(TestZjSingletonCtr, MultipleAccess)
-{
-    EXPECT_EQ(ZjSingletonCtrTest::getInstance(0).base(), 0);
-
-    auto& instance1 = ZjSingletonCtrTest::getInstance(0);
-    EXPECT_EQ(instance1.base(), 0);
-
-    auto& instance10Actually1 = ZjSingletonCtrTest::getInstance(123);
-    EXPECT_EQ(instance10Actually1.base(), 0);
-
-    ZjSingletonCtrTest::getInstance(133).addToBase(10);
-    EXPECT_EQ(ZjSingletonCtrTest::getInstance(-1).base(), 10);
-    EXPECT_EQ(instance1.base(), 10);
-    EXPECT_EQ(instance10Actually1.base(), 10);
-
-    addToSingletonCtrTest(3);
-    EXPECT_EQ(ZjSingletonCtrTest::getInstance(-30).base(), 13);
-    EXPECT_EQ(instance1.base(), 13);
-    EXPECT_EQ(instance10Actually1.base(), 13);
 }
