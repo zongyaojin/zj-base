9c9801738dc1dd5333bdf8ecc7621271fd6c1f62
Zongyao Jin <zongyaojin@outlook.com>
2023-08-07 23:48:51 -0700

    adds code coverage (#1)
    * added worked but messy
    
    * up
    
    * up
    
    * up
    
    * adjusts scripts

---
+++ scripts/zj-build-install-example.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Exit the script on any error
+set -e
+
+# Script absolute path
+script_abs_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Path definitions
+build_path=$script_abs_path/../build
+client_project_example_path=$script_abs_path/../client-project-example
+install_path=$client_project_example_path/install
+
+# Remove the build and install path for a fresh start
+rm -rf $build_path
+rm -rf $install_path
+
+# Make directories
+mkdir -p $build_path
+mkdir -p $install_path
+
+# Go to the build directory, build and install
+(
+    cd $build_path
+    cmake .. \
+        -D CMAKE_INSTALL_PREFIX=$install_path \
+        -D CMAKE_BUILD_TYPE=Release \
+        -D BUILD_SHARED_LIBS=OFF \
+        -D BUILD_TESTING=ON
+    make install
+    ctest
+)
