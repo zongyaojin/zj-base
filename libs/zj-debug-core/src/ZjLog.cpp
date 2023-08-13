3c1877cf9d39d9f7ce2fd9f672f29613bb5468e3
Zongyao Jin <zongyaojin@outlook.com>
2023-08-13 12:26:30 -0700

    up

--- libs/zj-debug-core/src/ZjLog.cpp
+++ libs/zj-debug-core/src/ZjLog.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file ZjLog.cpp
+ * @author Zongyao Jin (zongyaojin@outlook.com)
+ * @date 2023-08
+ * @copyright Copyright (c) 2023 by Zongyao Jin
+ */
+
 #include "ZjLog.hpp"
 #include "ZjColors.hpp"
 #include "ZjChrono.hpp"
